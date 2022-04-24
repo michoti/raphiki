bf7180fef94370abcd49cf4c228d6bc069f9c4b2
shukeel <50740328+shukeel@users.noreply.github.com>
2022-04-25 00:59:29 +0300

    modified messages

--- admin/message.php
+++ admin/message.php
@@ -17,7 +17,7 @@ include 'partials/head_admin.php';
     <section class="section dashboard">
         <div class="row">
            <div class="col-8 offset-2">
-           <table class="table">
+           <table class="table" id="stray-msg">
                 <thead>
                     <tr>
                         <th scope="col">Name</th>
@@ -35,19 +35,43 @@ include 'partials/head_admin.php';
                         
                         while ($row = $stmt->fetch_assoc()) { 
                     ?>
-                        <tr>
+                        <tr class="py-1" <?php if($row['status'] == 0){ echo 'style="background: #ccddff"';} ?>>
                         <th><?=  $row['name']?></th>
                         <td><?=  $row['email']?></td>
                         <td><?=  $row['subject']?></td>
                         <td><?=  $row['message']?></td>
-                        <td><?=  $row['date']?></td>
+                        <td><?=  $row['created_at']?></td>
                         <td>
-                           <a>Delete</a>
+                           <div class="d-flex justify-content-around">
+                            <form method="POST">
+                                <input type="hidden" name="id" value="<?= $row['id']?>">    
+                                <button class="btn" type="submit" name="read-msg"><i class="fa fa-envelope-open" aria-hidden="true"></i></button>
+                            </form>
+                            <form method="POST">
+                                <input type="hidden" name="id" value="<?= $row['id']?>">    
+                                <button class="btn" type="submit" name="delete-msg"><i class="fa fa-trash" aria-hidden="true"></i></button>
+                            </form>
+                           </div>
                         </td>
                         </tr>
                     <?php } ?>
                 </tbody>
             </table>
+            <?php
+              if(isset($_POST['read-msg']))
+              {
+                  $id = $_POST['id'];
+                  $query = "UPDATE stray_msg SET status = 1 WHERE id='$id'";
+                  $execute_stray_msg_status_update = $db->conn->query($query);
+              }
+
+              if(isset($_POST['delete-msg']))
+              {
+                  $id = $_POST['id'];
+                  $query = "DELETE FROM stray_msg WHERE id='$id'";
+                  $execute_stray_msg_delete = $db->conn->query($query);
+              }
+            ?>
            </div>
         </div>
     </section>
@@ -61,3 +85,11 @@ include 'partials/footer_admin.php';
 </body>
 
 </html>
+
+<script>
+    $(document).ready(function(){
+
+        $('#stray-msg').DataTable();
+
+    });
+</script>
